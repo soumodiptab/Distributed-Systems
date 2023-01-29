9d04947e0ad37e46bbb99151a494d4560feafecf
Soumodipta Bose <sbose2019@gmail.com>
2023-01-29 20:43:38 +0530

    test 1 working

--- assignment-1/2.cpp
+++ assignment-1/2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 const int MASTER = 0;
 // Defines the stucture of the particle
-typedef struct p
+typedef struct x
 {
     int x;
     int y;
@@ -12,11 +12,108 @@ typedef struct p
 
 void updateVector(Particle &p, int rows, int cols)
 {
-    
+    if (p.y == 0 && p.dir == 'D')
+    {
+        p.y = 1;
+        p.dir = 'U';
+        return;
+    }
+    else if (p.y == rows - 1 && p.dir == 'U')
+    {
+        p.y = rows - 2;
+        p.dir = 'D';
+        return;
+    }
+    else if (p.x == 0 && p.dir == 'L')
+    {
+        p.x = 1;
+        p.dir = 'R';
+        return;
+    }
+    else if (p.x == cols - 1 && p.dir == 'R')
+    {
+        p.x = cols - 2;
+        p.dir = 'L';
+        return;
+    }
+    else if (p.dir == 'U')
+    {
+        p.y++;
+    }
+    else if (p.dir == 'D')
+    {
+        p.y--;
+    }
+    else if (p.dir == 'L')
+    {
+        p.x--;
+    }
+    else if (p.dir == 'R')
+    {
+        p.x++;
+    }
+}
+string parseLoc(Particle &p)
+{
+    return to_string(p.x) + "|" + to_string(p.y);
 }
 
-void collisionResolution(vector<Particle> &particles, int rows, int cols)
+void collisionResolution(vector<Particle> &particles, int rows, int cols, int k)
 {
+    unordered_map<string, vector<Particle *>> collision_points;
+    for (int i = 0; i < k; i++)
+    {
+        collision_points[parseLoc(particles[i])].push_back(&particles[i]);
+    }
+    for (auto &p : collision_points)
+    {
+        if (p.second.size() >= 2)
+        {
+            //
+            cout << " <collided at " << p.first << "  >" << endl;
+            vector<bool> vis(p.second.size(), false);
+            for (int i = 0; i < p.second.size(); i++)
+            {
+                if (vis[i])
+                    continue;
+                for (int j = i + 1; j < p.second.size(); j++)
+                {
+                    if (p.second[i]->dir == 'R' && p.second[j]->dir == 'L' && (p.second[i]->y != 0 || p.second[i]->y == rows - 1))
+                    {
+                        p.second[i]->dir = 'D';
+                        p.second[j]->dir = 'U';
+                        vis[i] = true;
+                        vis[j] = true;
+                        break;
+                    }
+                    else if (p.second[i]->dir == 'L' && p.second[j]->dir == 'R' && (p.second[i]->y != 0 || p.second[i]->y == rows - 1))
+                    {
+                        p.second[i]->dir = 'U';
+                        p.second[j]->dir = 'D';
+                        vis[i] = true;
+                        vis[j] = true;
+                        break;
+                    }
+                    else if (p.second[i]->dir == 'U' && p.second[j]->dir == 'D' && (p.second[i]->x != 0 || p.second[i]->x == cols - 1))
+                    {
+                        p.second[i]->dir = 'R';
+                        p.second[j]->dir = 'L';
+                        vis[i] = true;
+                        vis[j] = true;
+                        break;
+                    }
+                    else if (p.second[i]->dir == 'D' && p.second[j]->dir == 'U' && (p.second[i]->x != 0 || p.second[i]->x == cols - 1))
+                    {
+                        p.second[i]->dir = 'L';
+                        p.second[j]->dir = 'R';
+                        vis[i] = true;
+                        vis[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
 int main(int argc, char *argv[])
 {
@@ -24,7 +121,9 @@ int main(int argc, char *argv[])
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // data type declare ----------------------------------------------------------
+    // ----------------------------------------------------------------------------
+
+    /* MPI PARTICLE : Custom PARTICLE TYPE */
     MPI_Datatype MPI_PARTICLE, oldtypes[2];
     MPI_Aint offsets[2], lb, extent;
     int blockcounts[2];
@@ -42,8 +141,9 @@ int main(int argc, char *argv[])
     vector<Particle> particles;
     if (rank == MASTER)
     {
-        // cin >> params[0] >> params[1] >> params[2] >> params[3];
-        params[0] = 5, params[1] = 5, params[2] = 3, params[3] = 1;
+        cin >> params[0] >> params[1] >> params[2] >> params[3];
+        //params[0] = 4, params[1] = 6, params[2] = 4, params[3] = 15;
+        cout << params[0] << " " << params[1] << " " << params[2] << " " << params[3] << endl;
         int total = params[2] + size - ((params[2]) % size);
         int elements = total / size;
         particles.resize(params[2]);
@@ -52,48 +152,50 @@ int main(int argc, char *argv[])
         //     // Particle p;
         //     cin >> particles[i].x >> particles[i].y >> particles[i].dir;
         // }
-        particles[0].x = 0, particles[0].y = 0, particles[0].dir = 'U';
-        particles[1].x = 0, particles[1].y = 1, particles[1].dir = 'D';
-        particles[2].x = 1, particles[2].y = 0, particles[2].dir = 'L';
+        // particles[0].x = 1, particles[0].y = 1, particles[0].dir = 'R';
+        // particles[1].x = 1, particles[1].y = 1, particles[1].dir = 'L';
+        // particles[2].x = 3, particles[2].y = 4, particles[2].dir = 'U';
+        // particles[3].x = 1, particles[3].y = 1, particles[3].dir = 'U';
         for (int i = 0; i < params[2]; i++)
         {
             // Particle p;
             cout << particles[i].x << " " << particles[i].y << " " << particles[i].dir << endl;
         }
     }
-    MPI_Bcast(params, 4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(params, 4, MPI_INT, MASTER, MPI_COMM_WORLD);
     int n = params[0], m = params[1], k = params[2], t = params[3];
-    int scatter_count = ceil(((double)k) / (double)size);
+    int scatter_count = ceil((double)k / size);
     // PARAM INIT
+    t--;
     while (t--) // for each time-slice
     {
         // Particle *sub_particles = (Particle *)malloc(sizeof(Particle) * scatter_count);
         // Particle sub_particles[scatter_count];
+        // if (rank == MASTER)
+        // {
+        //     scatter_count = ceil((double)particles.size() / size);
+        // }
+        // MPI_Bcast(&scatter_count, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
         vector<Particle> sub_particles(scatter_count);
         MPI_Scatter(particles.data(), scatter_count, MPI_PARTICLE, sub_particles.data(), scatter_count, MPI_PARTICLE, MASTER, MPI_COMM_WORLD);
-        // for (int i = 0; i < scatter_count; i++)
-        // {
-        //     cout << rank << "\t" << sub_particles[i].x << "\t" << sub_particles[i].y << "\t" << sub_particles[i].dir << endl;
-        // }
-        
-
+        for (int i = 0; i < scatter_count; i++)
+        {
+            // cout << rank << "\t" << sub_particles[i].x << "\t" << sub_particles[i].y << "\t" << sub_particles[i].dir << endl;
+            updateVector(sub_particles[i], m, n);
+        }
         MPI_Gather(sub_particles.data(), scatter_count, MPI_PARTICLE, particles.data(), scatter_count, MPI_PARTICLE, MASTER, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
         if (rank == MASTER)
         {
             cout << " gathered" << endl;
-            cout << sizeof(MPI_PARTICLE) << " " << sizeof(Particle) << endl;
-            for (int i = 0; i < params[2]; i++)
+            for (int i = 0; i < k; i++)
             {
                 // Particle p;
-                cout << " * " << particles[i].x << " " << particles[i].y << " " << particles[i].dir << endl;
+                cout << "* " << particles[i].x << " " << particles[i].y << " " << particles[i].dir << endl;
             }
+            // collision resolution
+            collisionResolution(particles, m, n, k);
         }
-    }
-    if (rank == MASTER)
-    {
-        cout << t << " here" << endl;
-        // delete (particles);
+        MPI_Barrier(MPI_COMM_WORLD);
     }
     MPI_Finalize();
     return 0;
---
+++ assignment-1/3.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <mpi.h>
+using namespace std;
+const int MASTER = 0;
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
