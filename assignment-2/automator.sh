8a4d1d45bf2497124c2a981ee61ddd645ae2031d
Soumodipta Bose <sbose2019@gmail.com>
2023-02-24 13:35:18 +0530

    done q1

--- assignment-2/automator.sh
+++ assignment-2/automator.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# docker exec -it namenode bash -c "apt update && apt install python3 -y"
-# docker exec -it datanode bash -c "apt update && apt install python3 -y"
-# docker exec -it resourcemanager bash -c "apt update && apt install python3 -y"
-# docker exec -it nodemanager bash -c "apt update && apt install python3 -y"
-# docker exec -it historyserver bash -c "apt update && apt install python3 -y"
-echo "Removing old input"
+echo "=== Executing program : $1 ==="
+echo "=== Removing old input ==="
 docker exec -it namenode bash -c "rm -r $1"
-echo "Copying files to namenode..."
+echo "=== Copying program : $1 to namenode ==="
 # docker cp mapper.py namenode:mapper.py
 # docker cp reducer.py namenode:reducer.py
 # docker cp input namenode:input
 # docker cp start.sh namenode:start.sh
 docker cp $1 namenode:$1
-echo "All files copied to namenode."
-echo "Running start.sh on namenode..."
+echo "=== All files copied to namenode. ==="
+echo "=== Running start.sh on namenode. ==="
 docker exec -it namenode bash -c "./$1/start.sh $1"
 cd ..
+echo "=== Finished executing program : $1 ==="
---
+++ assignment-2/env_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# docker exec -it namenode bash -c "apt update && apt install python3 -y"
+# docker exec -it datanode bash -c "apt update && apt install python3 -y"
+# docker exec -it resourcemanager bash -c "apt update && apt install python3 -y"
+# docker exec -it nodemanager bash -c "apt update && apt install python3 -y"
+# docker exec -it historyserver bash -c "apt update && apt install python3 -y"
