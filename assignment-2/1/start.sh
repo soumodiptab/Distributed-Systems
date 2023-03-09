9968c04e5afa8b4623af006028b00f041c35cbd3
Soumodipta Bose <sbose2019@gmail.com>
2023-03-09 10:35:41 +0530

    checkpoint

--- assignment-2/1/start.sh
+++ assignment-2/1/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #echo "Updating Python..."
 #apt update && apt install python3 -y
+# $1 = path to jar file $2 = path to input file $3 hdfs input dir $4 hdfs output dir $5 = mapreduce file loc
+
 cd $1
 rm -r output
 rm -r input
@@ -22,6 +24,8 @@ hadoop fs -mkdir -p input
 echo "=== Copying input files to HDFS... ==="
 hdfs dfs -put input/* input
 echo "=== Running MapReduce JOB1 ==="
+# alternate command
+#hadoop jar /opt/hadoop-3.2.1/share/hadoop/tools/lib/hadoop-streaming-3.2.1.jar -files mapper2.py,reducer2.py  -mapper 'python3 mapper2.py' -reducer 'python3 reducer2.py' -input input -output output
 mapred streaming -files mapper.py,reducer.py -mapper 'python3 mapper.py' -reducer 'python3 reducer.py' -input input -output output
 echo "=== MapReduce job completed. ==="
 rm -r input
